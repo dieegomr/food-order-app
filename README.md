a3d981d5cd8421b17bf9effeb9d463e0bd241fdd
dieegomr <dmendes.rocha@gmail.com>
2022-07-30 17:30:52 -0300

    readme added, data moved to back end, Loading message  and error handling implemented

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1>Food Order App 👨🏼‍🍳🍔🍕</h1>
+
+<p><a href="https://github.com/dieegomr/food-order-app/blob/main/README_pt-BR.md">README.md</a> em português</p>
+<p>
+This is a food order application where it has a list of meals from which user can choose and he can then add these meals to the cart and later manage the cart adding more dishes or changing the quantity.</p>
+<p>Food order app is part of <a href="https://www.udemy.com/share/101Way3@Tl-LkQQtUAOqtTxMOnWfVxtvmAXWum0zMXkgJeQeEA3ZxE_NFZ1NpKMcjaQF9vuqrQ==/">"React - The Complete Guide (incl Hooks, React Router, Redux)
+"</a>.</p>
+<p>It was possible to practice components, React Hooks, render lists and conditional content.
+</p>
+
+<p>
+The HTML and CSS were ready, but it was necessary to follow the to implement the JSX code for the site to work properly. </p>
+
+<h3>Sample:</h3>
+<p>Click <a href="">here!</a> to access the website</p>
+<img src="" alt="website gif" width="800" height="500">
+
+<h3>How to run this website?</h3>
+
+<p> 1) Clone the repository </p>
+<p>     $ git clone https://github.com/dieegomr/forkfy-app.git</p>
+<p> 2) Open the project folder on the terminal and run the following command</p>
+<p>     $ npm install</p>
+<p>     $ npm start</p>
+<p> 3) Check the message: "Server will be running at http://localhost:XXXX"</p>
+<p> 4) Copy and paste the link on your Browser</p>
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,47 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useState, useEffect } from 'react';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        'https://react-http-ad80e-default-rtdb.firebaseio.com/meals.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went worng!');
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHasError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -43,7 +54,9 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {hasError && <p className={classes.ErrorMessage}>{hasError}</p>}
+        {isLoading && <p className={classes.MealsLoading}>Loading...</p>}
+        {!isLoading && <ul>{mealsList}</ul>}
       </Card>
     </section>
   );
